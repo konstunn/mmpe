7790e660c45821a8cc2341a75e15d5c5828e0e73
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-10-07 12:15:07 +0700

    Update test

--- 1/7/model/test.py
+++ 1/7/model/test.py
@@ -1,17 +1,27 @@
 from model import Model
+import numpy as np
 
-F = lambda th: th[0]
-C = lambda th: th[1]
-G = lambda th: 1
-H = lambda th: 1
-X0 = lambda th: 0
-P0 = lambda th: 0.1
-Q = lambda th: 0.1
-R = lambda th: 0.3
+F = lambda th: [[th[0], 1],
+                [0, .5]]
+
+C = lambda th: [[th[1]], [1]]
+
+G = lambda th: np.eye(2)
+H = lambda th: [1, 0]
+X0 = lambda th: [0, -1]
+P0 = lambda th: 0.1 * np.eye(2)
+Q = lambda th: 0.1 * np.eye(2)
+R = lambda th: 0.1
 u = [2., 2.]
-th = [1., 1.]
+th = [.5, .5]
 
 m = Model(F, C, G, H, X0, P0, Q, R, th)
+
 # m.fim(u)
-# 8.35  12.31
-# 12.31 27.70
+
+n = np.array(F(th)).shape[0]
+s = len(th)
+s2q = lambda s: int((s + 1) * s / 2 + 1)
+q = s2q(s)
+x = np.random.uniform(-1, 1, q*s).reshape([q, n])
+p = [1/q] * q
