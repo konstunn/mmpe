e8bbcb8f6be6a42d44b7e68b5962e86a4960bce8
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-12-10 22:12:51 +0700

    update, fix tf matderiv function

--- 2/3/model/jac.py
+++ 2/3/model/jac.py
@@ -1,41 +1,49 @@
 
 import tensorflow as tf
 
-x = [2., 3.]
+x = [1., 2.]
 s = len(x)
 x = tf.constant(x)
 
-a = [[2*x[0]*x[1], 3*x[0]*x[1]],
-     [4*x[0]*x[1], 5*x[0]*x[1]]]
+a = [[2*x[0]*x[1]],
+     [4*x[0]*x[1]]]
 
 a = tf.convert_to_tensor(a)
 
-n = tf.size(a)
 
-a = tf.reshape(a, [n])
+# derivative of matrix 'a' w.r.t vector 'x'
+def matderiv(a, x):
+    mn = tf.size(a)
+    m, n = a.get_shape()
+
+    a = tf.reshape(a, [mn])
+
+    def cond(i, N, rez):
+        return tf.less(i, N)
+
+    def body(i, N, rez):
+        elem = tf.slice(a, [i], [1])
+        elem = tf.gradients(elem, x)
+        elem = tf.reshape(elem, [s, 1])
+        rez = tf.concat([rez, elem], 1)
+        i = i + 1
+        return i, N, rez
+
+    shape_invariants = [tf.TensorShape([]),
+                        tf.TensorShape([]),
+                        tf.TensorShape([s, None])]
+
+    elem = tf.slice(a, [0], [1])
+    rez = tf.gradients(elem, x)[0]
+    rez = tf.reshape(rez, [s, 1])
+
+    loop = tf.while_loop(cond, body, [1, mn, rez], shape_invariants)
+
+    rez = loop[2]
+
+    # FIXME
+    rez = tf.reshape(rez, [s, m, n])
+    return rez
 
 
-def cond(i, N, rez):
-    return tf.less(i, N)
-
-
-def body(i, N, rez):
-    elem = tf.slice(a, [i], [1])
-    elem = tf.gradients(elem, x)
-    elem = tf.reshape(elem, [s, 1])
-    rez = tf.concat([rez, elem], 1)
-    i = i + 1
-    return i, N, rez
-
-
-shape_invariants = [
-                    n.get_shape(),
-                    n.get_shape(),
-                    tf.TensorShape([s, None])
-]
-
-elem = tf.slice(a, [0], [1])
-rez = tf.gradients(elem, x)[0]
-rez = tf.reshape(rez, [s, 1])
-
-loop = tf.while_loop(cond, body, [1, n, rez], shape_invariants)
+rez = matderiv(a, x)
